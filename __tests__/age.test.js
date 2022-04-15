4e495d23bead312f7ed2802346a31874a3f5ff43
William Rodriguez <william@rodriguez1.com>
2022-04-15 15:26:40 -0700

    Add test for dog years

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -4,7 +4,7 @@ describe("age", () => {
   let age;
 
   beforeEach(() => {
-    age = new Age(30);
+    age = new Age(30, false);
   });
   test("It should return an object with the given age as a property", () => {
     expect(age.years).toEqual(30);
@@ -31,13 +31,20 @@ describe("age", () => {
   });
 
   test("If they are older than their life expectancy, this should return the number of years they have lived past it", () => {
-    age = new Age(100);
+    age = new Age(100, false);
     expect(age.averageLifeExpectancy(80)).toEqual({"mercury": 83, "venus": 32, "mars": 11, "jupiter": 2});
   });
-});
 
-/*
-  test("", () => {
-    
+  test("If dogYears is true it should perform all calculations in dog years instead of human years", () => {
+    age = new Age(30, true);
+    expect(age.years).toEqual(210);
+    expect(age.mercury()).toEqual(875);
+    expect(age.venus()).toEqual(339);
+    expect(age.mars()).toEqual(112);
+    expect(age.jupiter()).toEqual(18);
+    expect(age.averageLifeExpectancy(80)).toEqual({"mercury": 1458, "venus": 565, "mars": 186, "jupiter": 30});
+
+    age = new Age(100, true);
+    expect(age.averageLifeExpectancy(80)).toEqual({"mercury": 583, "venus": 226, "mars": 74, "jupiter": 12});
   });
-*/
+});
